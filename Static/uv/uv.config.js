4dcc5ee17938e2af30059bac3cbd1f9f3c2fb125
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-10-05 10:08:57 -0400

    Update uv.config.js

--- Static/uv/uv.config.js
+++ Static/uv/uv.config.js
@@ -1,36 +1,6 @@
-const xor = {
-    encode(str) {
-      if (!str) return str;
-      let result = "";
-      for (let i = 0; i < str.length; i++) {
-        result += i % 4 ? String.fromCharCode(str.charCodeAt(i) ^ 2) : str[i];
-      }
-      return encodeURIComponent(result);
-    },
-    decode(str) {
-      if (!str) return str;
-      const [input, ...search] = str.split("?");
-      let result = "";
-      const decoded = decodeURIComponent(input);
-      for (let i = 0; i < decoded.length; i++) {
-        result +=
-          i % 4 ? String.fromCharCode(decoded.charCodeAt(i) ^ 2) : decoded[i];
-      }
-      return result + (search.length ? "?" + search.join("?") : "");
-    },
-};
-
-
-/*global Ultraviolet*/
-
 self.__uv$config = {
-    prefix: '/uv/service/',
-    bare: '/bare/',
-    encodeUrl: Ultraviolet.codec.base64.encode,
-    decodeUrl: Ultraviolet.codec.base64.decode,
-    handler: '/uv/uv.handler.js',
-    client: '/uv/uv.client.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+  prefix: '/uv/service/',  // Proxy prefix
+  bare: '/bare/',  // Bare server path
+  encodeUrl: (url) => btoa(url),  // Base64 encode function
+  decodeUrl: (url) => atob(url),  // Base64 decode function
 };
