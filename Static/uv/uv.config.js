30edfe2cbb3e94b01b2ea58d1d40d0ae3b6ae118
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-10-05 10:16:36 -0400

    Delete Static/uv directory

--- Static/uv/uv.config.js
+++
@@ -1,6 +0,0 @@
-self.__uv$config = {
-  prefix: '/uv/service/',  // Proxy prefix
-  bare: '/bare/',  // Bare server path
-  encodeUrl: (url) => btoa(url),  // Base64 encode function
-  decodeUrl: (url) => atob(url),  // Base64 decode function
-};
