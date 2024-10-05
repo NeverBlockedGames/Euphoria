fd2257c7e6ea6024dbfd22a6651b3ca8d8e5fce4
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-10-05 13:01:30 -0400

    Update register-sw.js

--- Static/uv/register-sw.js
+++ Static/uv/register-sw.js
@@ -1,6 +1,6 @@
 // register-sw.js
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/Static/uv/uv.sw.js')
+  navigator.serviceWorker.register('/uv/uv.sw.js')
     .then(registration => {
       console.log('Service worker registered:', registration);
     })
